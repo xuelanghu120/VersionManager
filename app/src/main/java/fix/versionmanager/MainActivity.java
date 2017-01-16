ccc88eb44e146ee2beb422da9970061011c41b29
xuzhu <xuzhu@51huxin.com>
2017-01-16 13:54:21 +0800

    创建tag

--- app/src/main/java/fix/versionmanager/MainActivity.java
+++ app/src/main/java/fix/versionmanager/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第一个版本
+        //创建了一个tag
     }
 }
